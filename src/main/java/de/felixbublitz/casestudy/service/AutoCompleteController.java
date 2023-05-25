85d21d7f2262f5119adb415c9556196d61184391
fbublitz <felixbublitz@googlemail.com>
2023-05-26 00:17:46 +0200

    implement HTTP request handling

--- src/main/java/de/felixbublitz/casestudy/service/AutoCompleteController.java
+++ src/main/java/de/felixbublitz/casestudy/service/AutoCompleteController.java
@@ -1,15 +1,26 @@
 package de.felixbublitz.casestudy.service;
 
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class AutoCompleteController {
 
-    @RequestMapping("/api/v1/auto-complete/")
-    String showDefaultMessage() {
-        return "service running";
-    }
+	private final Database db;
 
+	public AutoCompleteController(Database db) {
+		this.db = db;
+	}
+
+	@RequestMapping(value = "/api/v1/auto-complete/{term}", produces = { "application/json" })
+	public @ResponseBody String getAttr(@PathVariable("term") String term) {
+		return db.getSearchResult(term).toString();
+	}
+
+	@RequestMapping("/api/v1/auto-complete/")
+	public @ResponseBody String showDefaultMessage() {
+		return "service running";
+	}
 }
