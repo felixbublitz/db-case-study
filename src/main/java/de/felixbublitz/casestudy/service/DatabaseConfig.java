f32b74c84668f38221d1c143a5e5cc18a6c29e54
fbublitz <felixbublitz@googlemail.com>
2023-05-26 00:18:10 +0200

    add DatabaseConfig

---
+++ src/main/java/de/felixbublitz/casestudy/service/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package de.felixbublitz.casestudy.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+	@Bean
+	public Database database() {
+		Database db;
+		
+		try {
+			db = new Database("D_Bahnhof_2016_01_alle.csv");
+			return db;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+}
