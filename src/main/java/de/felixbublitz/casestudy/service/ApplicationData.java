c8534f5d57598f9b63732ee381c584b61b235134
fbublitz <felixbublitz@googlemail.com>
2023-05-26 00:16:20 +0200

    refactoring

--- src/main/java/de/felixbublitz/casestudy/service/ApplicationData.java
+++ src/main/java/de/felixbublitz/casestudy/service/ApplicationData.java
@@ -1,15 +1,12 @@
 package de.felixbublitz.casestudy.service;
 
 public class ApplicationData {
-	
-	public static Database db;
 	public final static int CSV_EVA = 0;
 	public final static int CSV_DS = 1;
 	public final static int CSV_NAME = 2;
 	public final static int CSV_VERKEHR = 3;
-	public final static  int CSV_LAENGE = 4;
+	public final static int CSV_LAENGE = 4;
 	public final static int CSV_BREITE = 5;
 	public final static String JSON_STATION_SIZE = "number_of_stations_found";
 	public final static String JSON_STATION_LIST = "station_list";
-
 }
--- src/main/java/de/felixbublitz/casestudy/service/Database.java
+++ src/main/java/de/felixbublitz/casestudy/service/Database.java
@@ -1,48 +1,44 @@
 package de.felixbublitz.casestudy.service;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.concurrent.TimeUnit;
-
 import org.json.JSONObject;
 
-
 public class Database {
-	
+
 	DatabaseNode rootNode;
-	
+	private double NS_TO_MS = 0.000000001;
+
 	public Database(String fileName) throws Exception {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(fileName)));
-	        
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(getClass().getClassLoader().getResourceAsStream(fileName)));
+
 		Stack<String[]> rawData = new Stack<String[]>();
 		String line;
-		
-		reader.readLine(); //skip title line in csv
+
+		reader.readLine(); // skip title line in csv
 		while ((line = reader.readLine()) != null) {
-	        String[] values = line.split(";");
-	        rawData.push(values);
-	   	}
-		 
+			String[] values = line.split(";");
+			rawData.push(values);
+		}
+
 		rootNode = new DatabaseNode(rawData, 0);
 	}
-	
-	
+
 	public JSONObject getSearchResult(String term) {
 		long startTime = System.nanoTime();
 
 		JSONObject out = new JSONObject();
-		
+
 		JSONObject result = rootNode.getSearchResult(term);
-		out.append(ApplicationData.JSON_STATION_LIST, result.get(ApplicationData.JSON_STATION_LIST));
-		out.append(ApplicationData.JSON_STATION_SIZE, result.get(ApplicationData.JSON_STATION_SIZE));
-		
-		
+		out.put(ApplicationData.JSON_STATION_LIST, result.getJSONArray(ApplicationData.JSON_STATION_LIST));
+		out.put(ApplicationData.JSON_STATION_SIZE, result.getInt(ApplicationData.JSON_STATION_SIZE));
+
 		long deltaTime = System.nanoTime() - startTime;
-		out.append("time_taken", String.format("%d ms", TimeUnit.NANOSECONDS.toMillis(deltaTime)));
+		out.put("time_taken", String.format("%f ms", deltaTime * NS_TO_MS).replace(',', '.'));
 		return out;
-		
+
 	}
 
 }
